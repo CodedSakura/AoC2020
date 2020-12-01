a6d16d1b8c7256e06789394ffc779a5b610a2d8f
codedsakura <sakura.coded@gmail.com>
2020-12-02 00:09:10 +0200

    slight optimisation

--- 01/a.py
+++ 01/a.py
@@ -1,7 +1,7 @@
 with open("./input.txt") as f:
     arr = [int(i) for i in f.readlines()]
-    for i in arr:
-        for j in arr:
-            if i + j == 2020:
-                print(i * j)
+    for i in range(len(arr)-1):
+        for j in range(i+1, len(arr)):
+            if arr[i] + arr[j] == 2020:
+                print(arr[i] * arr[j])
                 break
--- 01/b.py
+++ 01/b.py
@@ -1,8 +1,7 @@
 with open("./input.txt") as f:
     arr = [int(i) for i in f.readlines()]
-    for i in arr:
-        for j in arr:
-            for k in arr:
-                if i + j + k == 2020:
-                    print(i * j * k)
-                    break
+    for i in range(len(arr)-2):
+        for j in range(i+1, len(arr)-1):
+            for k in range(j+1, len(arr)):
+                if arr[i] + arr[j] + arr[k] == 2020:
+                    print(arr[i] * arr[j] * arr[k])
